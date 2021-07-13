301eda9aa78132da08d004427281aedcaea7b9ac
User <hjy5421@naver.com>
2021-07-13 23:34:45 +0900

    add member repository, service

---
+++ server/src/test/java/com/projects/server/service/MemberServiceTest.java
@@ -0,0 +1,25 @@
+package com.projects.server.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService=new MemberService();
+
+    @Test
+    void join() {
+        //given
+        //when
+        //then
+    }
+
+    @Test
+    void findMembers() {
+    }
+
+    @Test
+    void findOne() {
+    }
+}
